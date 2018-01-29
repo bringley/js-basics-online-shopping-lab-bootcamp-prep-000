1b0e840f57972d526cb10eb6b2926592442de3da
Brian Ringley <brian.ringley@wework.com>
2018-01-29 20:31:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -63,7 +63,9 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber === null) {
+  if cardNumber.isNaN() {
     msg = `Sorry, we don't have a credit card on file for you.`
+  } else {
+    msg = `Your total cost is $${total(cart)}, which will be charged to the card ${cardNumber}.`
   }
 }
