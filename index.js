f6e5d54ca73dda4623518ecc424c9ccef8a7bfd8
Brian Ringley <brian.ringley@wework.com>
2018-01-29 19:57:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,21 +16,6 @@ function addToCart(name) {
   return cart
 }
 
-/*function viewCart() {
-  var msg = 'In your cart, you have'
-  if (cart.length === 0){
-    msg = 'Your shopping cart is empty.'
-  } else if (cart.length === 1) {
-    msg = msg + ` ${Object.keys(cart[0])} at $${Object.values(cart[0])}.`
-    } else {
-      for (let i = 0; i < cart.length - 1; i++) {
-        msg = msg + ` ${Object.keys(cart[i])} at $${Object.values(cart[i])},`
-      }
-      msg = msg + ` and ${Object.keys(cart[cart.length - 1])} at $${Object.values(cart[cart.length - 1])}.`
-    }
-  console.log(msg)
-}*/
-
 function viewCart() {
   var msg = 'In your cart, you have'
   if (cart.length === 0){
